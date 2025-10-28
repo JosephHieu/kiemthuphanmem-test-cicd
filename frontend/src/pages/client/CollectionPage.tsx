59fec0ddf1c59ab1dc043382a1b35fd238de9875
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-10-28 16:11:32 +0700

    comment code thừa frontend

--- frontend/src/pages/client/CollectionPage.tsx
+++ frontend/src/pages/client/CollectionPage.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
 function CollectionPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default CollectionPage
+export default CollectionPage;
